626e5a06e68ecb71eec1856b444f53e44a7ebbae
Anastasia Tierokhina <anastasia.avanheim@gmail.com>
2022-07-27 00:08:44 +0300

    final commit Object this and methods

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-export const hotel = new Object();
+export const hotel = {
+  quantityOfPlaces: 30,
+  priceByPlace: 20,
+  bankAccount: 0,
+  guests: {},
+  getLength: function () {
+    return Object.keys(this.guests).length;
+  },
+  getActualFreePlace: function () {
+    return this.quantityOfPlaces - this.getLength();
+  },
+  paidPerPlace: function () {
+    return (this.bankAccount += this.priceByPlace);
+  },
+  checkInGuest: function (firstName, lastName, money) {
+    if (this.getActualFreePlace() === 0) {
+      return 'Sorry, we have not free spaces';
+    } else if (money < this.priceByPlace) {
+      return 'Sorry, you have not enough money';
+    }
+    this.guests[this.getLength()] = {
+      firstName,
+      lastName,
+      money: money - this.priceByPlace,
+    };
+    this.paidPerPlace();
+    return this.guests[this.getLength() - 1];
+  },
+};
